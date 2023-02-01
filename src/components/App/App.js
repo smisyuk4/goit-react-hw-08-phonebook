e94745f8988dacb4134a2fb49e6d646fa9c49317
Sergey <smisyuk@gmail.com>
2023-02-01 23:35:12 +0200

    add header and route

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+import { Header } from "components/Header";
+import { Home } from "../../pages"
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Header/>}>
+        <Route index element={<Home/>}/>
+      </Route>
+    </Routes>
+  );
+};
--- src/task.md
+++ src/task.md
@@ -34,6 +34,7 @@ npm install react-router-dom
 
 https://mui.com/
 
+npm install @mui/material @emotion/react @emotion/styled
 npm install @mui/material @mui/styled-engine-sc styled-components
 npm install @mui/icons-material
 
