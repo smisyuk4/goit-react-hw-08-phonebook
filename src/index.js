0e49ef4720788a8152a865cfbfe8ba088bcb0c6a
Sergey <smisyuk@gmail.com>
2023-02-07 01:32:32 +0200

    add redux-persist to store and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from "react-redux";
-import { store } from 'redux/store'; 
+import { store, persistor } from 'redux/store'; 
 
 
 import { App } from 'components/App';
@@ -11,9 +12,11 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">      
-        <App />      
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">      
+          <App />      
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,20 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { apiSlice } from "./auth/apiSlice"
 import { authReducer } from "./auth/authSlice"
+import { persistStore, persistReducer} from 'redux-persist' //, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER 
+import storage from 'redux-persist/lib/storage'
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whileList: ['token'],
+}
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    auth: authReducer,
+    // auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer)
   },
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware(),
@@ -16,3 +25,5 @@ export const store = configureStore({
 })
 
 setupListeners(store.dispatch)
+
+export const persistor = persistStore(store)
