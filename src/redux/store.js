dbb0e0ba5e9d948be7e3cb8a231618c74052f26f
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-02-07 09:25:13 +0200

    Fix persistConfig
    While -> whiteList

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,7 @@ import storage from 'redux-persist/lib/storage'
 const authPersistConfig = {
   key: 'auth',
   storage,
-  whileList: ['token'],
+  whiteList: ['token'],
 }
 
 export const store = configureStore({
