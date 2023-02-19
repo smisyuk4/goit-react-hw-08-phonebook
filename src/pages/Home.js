86bdd2e58c2999fc139a455bfd3a931497301cd0
Sergey <smisyuk@gmail.com>
2023-02-20 01:25:41 +0200

    write info on home page an remove favicon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
 const Home = () => {
     return (
         <>
-            <h1>Home page</h1>
+            <h1>React app</h1>            
+            <ul>
+                <li><a href='https://www.linkedin.com/in/%D1%81%D0%B5%D1%80%D0%B3%D0%B5%D0%B9-%D0%BC%D0%B8%D1%81%D1%8E%D0%BA'>linkedin</a></li>
+                <li><a href='https://github.com/smisyuk4/goit-react-hw-08-phonebook'>github</a></li>
+                <li><a href='https://connections-api.herokuapp.com/docs/#/User/post_users_signup'>connections-api</a></li>
+                <li><b>Usage:</b></li>
+                <ul>                    
+                    <li>react-redux</li>
+                    <li>reduxjs/toolkit</li>
+                    <li>react-router-dom</li>
+                    <li>redux-persist</li>
+                    <li>formik</li>
+                    <li>formik-mui</li>
+                    <li>emotion/styled</li>
+                    <li>mui/styled</li>
+                    <li>mui/icons-material</li>
+                    <li>yup</li>
+                    <li>prop-types</li>
+                    <li>notiflix</li>
+                </ul>
+                <li><b>Resources:</b></li>
+                <ul>
+                    <li>official docs</li>
+                    <li>developer.mozilla.org</li>
+                    <li>stackoverflow.com</li>                
+                </ul>
+            </ul>
         </>
     )
 }
